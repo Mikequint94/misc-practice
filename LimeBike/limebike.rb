46ebc83ea2a1ba5337a3090c4489d5ed40b32c7e
Michael Quint <mjq8@cornell.edu>
2017-10-29 13:50:07 -0700

    working code

--- LimeBike/limebike.rb
+++ LimeBike/limebike.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class ItemCounter
 
   def initialize
@@ -6,19 +8,25 @@ class ItemCounter
     @ride_end = []
     #rides a hash of start, end, and items
     @rides = []
+    #items an array of hashes to save space and time by not repeating, just referencing
+    @items = {}
+    @ride_number = 1
   end
 
   def process_ride(ride)
     #assuming that all rides will be processed then print items happens just once.
     #Therefore sorting only happens once
     #compared to all the inserts so its less time consuming
-    @ride_start.push({ride[:start_time] => ride[:items]})
-    @ride_end.push({ride[:end_time] => ride[:items]})
+
+    @ride_start.push(ride[:start_time] => @ride_number)
+    @ride_end.push(ride[:end_time] => @ride_number)
+    @items[@ride_number] = ride[:items]
+    @ride_number +=1
   end
 
   def print_items_per_interval()
     finish_processing
-    @rides[1..-1].each do |ride|
+    @rides.each do |ride|
       start_time = format_times(ride[:start_time])
       end_time = format_times(ride[:end_time])
       items = format_items(ride[:items])
@@ -27,45 +35,33 @@ class ItemCounter
   end
 
   def finish_processing
+    # debugger
     @ride_start = @ride_start.sort_by {|ride| ride.keys}
     @ride_end = @ride_end.sort_by {|ride| ride.keys}
-    current_items = []
+    shift_starting = @ride_start.shift
+    current_items = [shift_starting.values]
     until @ride_start.empty?
       if @ride_start[0].keys[0] < @ride_end[0].keys[0]
-        shift_starting = @ride_start.shift
-        shift_ending ||= shift_starting
-        current_items += shift_ending.values[0]
-      else
-        shift_starting = @ride_end.shift
-        shift_ending ||= shift_starting
-        current_items -= shift_ending.values[0]
-      end
-      @rides.push(start_time: shift_ending.keys[0], end_time: shift_starting.keys[0], items: current_items)
-      if @ride_start.length > 0
-        if @ride_start[0].keys[0] < @ride_end[0].keys[0]
-          shift_ending = @ride_start.shift
-          # current_items += shift_starting.values[0]
-        else
-          shift_ending = @ride_end.shift
-        end
+        shift_ending = @ride_start.shift
+        item_operator = :+
       else
         shift_ending = @ride_end.shift
+        item_operator = :-
       end
       @rides.push(start_time: shift_starting.keys[0], end_time: shift_ending.keys[0], items: current_items)
+      shift_starting = shift_ending
+      current_items = current_items.send(item_operator, [shift_starting.values])
     end
     until @ride_end.empty?
-      shift_starting = @ride_end.shift
-      current_items -= shift_starting.values[0]
-      @rides.push(start_time: shift_ending.keys[0], end_time: shift_starting.keys[0], items: current_items)
       shift_ending = @ride_end.shift
-      current_items -= shift_ending.values[0]
       @rides.push(start_time: shift_starting.keys[0], end_time: shift_ending.keys[0], items: current_items)
+      shift_starting = shift_ending
+      current_items -= [shift_starting.values]
     end
   end
 
   def format_times(time)
     time = time.to_s
-
     if time.length == 3
       time = "0" + time
     end
@@ -74,8 +70,10 @@ class ItemCounter
 
   def format_items(items)
     item_hash = Hash.new(0)
-    items.each do |item|
-      item_hash[item] += 1
+    items.each do |item_bundle|
+      @items[item_bundle[0]].each do |item|
+        item_hash[item] += 1
+      end
     end
     item_with_count = []
     item_hash.each do |item, count|
