498eeab9ab42e170b2e0c7189dbd4f918ea99e9c
Michael Quint <mjq8@cornell.edu>
2017-11-10 11:06:03 -0800

    starting project boilerplate

---
+++ Menu_Component/menu-item/src/frontend/components/menu/menu_index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MenuIndex extends Component {
+  render() {
+    return (
+      <div><h1> Here are the menu items of the day</h1>
+      <h2> Click each item to view ingredients </h2>
+    );
+  }
+}
+
+export default MenuIndex;
