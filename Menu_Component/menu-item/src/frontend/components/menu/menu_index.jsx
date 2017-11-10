885d946b43d4cbc9188c16d873a48949b1c348a9
Michael Quint <mjq8@cornell.edu>
2017-11-10 12:55:34 -0800

    functioning

--- Menu_Component/menu-item/src/frontend/components/menu/menu_index.jsx
+++ Menu_Component/menu-item/src/frontend/components/menu/menu_index.jsx
@@ -3,10 +3,28 @@ import MenuItems from '../../../data/seed.js';
 import MenuIndexItem from './menu_index_item';
 
 class MenuIndex extends Component {
+  constructor(){
+    super();
+    this.state={showIngredients: ""};
+
+    // this.showIngredients = this.showIngredients.bind(this);
+  }
+
+  showIngredients(name){
+    if (this.state.showIngredients === name) {
+      this.setState({showIngredients: ""});
+    } else {
+      this.setState({showIngredients: name});
+    }
+  }
+
   render() {
     let items = MenuItems.map((item) =>
-      <MenuIndexItem key={"item"+item.name} item={item}/>
+      <div key={"item"+item.name} onClick={this.showIngredients.bind(this, item.name)}>
+        <MenuIndexItem show={this.state.showIngredients} item={item}/>
+      </div>
     );
+
     return (
       <div>
         <h1> Here are the menu items of the day</h1>
--- Menu_Component/menu-item/src/frontend/components/menu/menu_index_item.js
+++ Menu_Component/menu-item/src/frontend/components/menu/menu_index_item.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 
 class MenuIndexItem extends Component {
-  constructor(props){
-    super(props);
-    this.state = {showIngredients: false};
-  }
+  // constructor(props){
+  //   super(props);
+  //   this.state = {showIngredients: false};
+  // }
 
-  toggleIngredients(){
-    if (this.state.showIngredients){
-      this.setState({showIngredients: false});
-    } else {
-      this.setState({showIngredients: true});
-    }
-  }
+  // toggleIngredients(){
+  //   if (this.state.showIngredients){
+  //     this.setState({showIngredients: false});
+  //   } else {
+  //     this.setState({showIngredients: true});
+  //   }
+  // }
   render() {
     let ingredients;
-    if (this.state.showIngredients) {
+    if (this.props.show === this.props.item.name) {
       ingredients = this.props.item.ingredients.map(
         (ingredient) => <li key={"ingredient"+ingredient}>{ingredient}</li>);
     }
+
     return (
       <div className="Menu-item">
-        <button onClick={this.toggleIngredients.bind(this)}> {this.props.item.name} </button>
+        <button> {this.props.item.name} </button>
         <ul>{ingredients}</ul>
       </div>
     );
