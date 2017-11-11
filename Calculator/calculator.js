3a5ae6661664b10c35f899fa2317fb7ed7744c56
Michael Quint <mjq8@cornell.edu>
2017-11-10 21:12:38 -0800

    Pemdas approval

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -19,7 +19,6 @@ const calculate = (string) => {
    });
    numbers.push(currentNumber.join(""));
    currentNumber = [];
-   console.log(numbers, operators);
    let sum = 0;
    let num1;
    let num2;
@@ -34,20 +33,14 @@ const calculate = (string) => {
       operators.splice(index,1);
     }
   });
-     console.log(numbers, operators);
   operators.forEach((operator, index) => {
     if (operator === "+") {
       numbers[index+1] = parseInt(numbers[index]) + parseInt(numbers[index+1]);
-      // numbers.splice(index+1, 1);
-      // operators.splice(index,1);
     } else if (operator === "-") {
       numbers[index + 1] = parseInt(numbers[index]) - parseInt(numbers[index+1]);
-      // numbers.splice(index+1, 1);
-      // operators.splice(index,1);
     }
   });
    console.log(numbers.pop());
-  // console.log(num1)
 };
 
-calculate("3+3+-2--12+3+2*2");
+calculate("3/3+-2--12/6+3+2*2");
