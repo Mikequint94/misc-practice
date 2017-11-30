33ac58a6ad82756067aa6354a2ea6a3a565a6099
Michael Quint <mjq8@cornell.edu>
2017-11-29 23:00:40 -0800

    Aurora Solar challenge

---
+++ AuroraSolar/index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+const fetch = require("request");
+
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.render('index.html');
+})
+let bodyText;
+const options = { method: 'POST',
+  url: 'https://aurorasolar-monitoring-task.herokuapp.com/plant_overview',
+  headers: 
+  { 'cache-control': 'no-cache',
+  'content-type': 'application/x-www-form-urlencoded' },
+  form: 
+  { password: '76adb77b86d44e7a9f6addde889553df',
+  candidate: 'HHAB14132' }
+};
+
+fetch(options, (error, response, body) => {
+  if (error) throw new Error(error);
+  bodyText = JSON.parse(body);
+});
+app.get('/plant_overview', function(req, res) {
+    res.json(bodyText);
+})
+app.get('/component_list', (req, res) => {
+  let components = [];
+  bodyText.forEach(entity => components.push({type: entity.type, name: entity.device_name}));
+  res.json(components);
+})
+app.get(`/channel_list/:component`, (req, res) => {
+  let componentChannels = bodyText.filter(
+    entity => entity.device_name == req.params.component
+  );
+  res.json(componentChannels[0]);
+})
+app.get(`/data_point/:device/:channel`, (req, res) => {
+  
+  const options = { method: 'POST',
+    url: 'https://aurorasolar-monitoring-task.herokuapp.com/entity_data',
+    headers: 
+     { 'cache-control': 'no-cache',
+       'content-type': 'application/x-www-form-urlencoded' },
+    form: 
+     { password: '76adb77b86d44e7a9f6addde889553df',
+       candidate: 'HHAB14132',
+       entity: req.params.device,
+       channel: req.params.channel
+      } };
+      
+  fetch(options, (error, response, body) => {
+    if (error) throw new Error(error);
+    res.json(body);
+  });
+       
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+
+
+
