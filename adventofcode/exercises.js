5429e4b97a736713c8d10d96d799522821b04952
Michael Quint <mjq8@cornell.edu>
2017-12-02 12:59:57 -0800

    checksum divisor

--- adventofcode/exercises.js
+++ adventofcode/exercises.js
@@ -34,3 +34,25 @@ function checkSum(spreadsheet) {
   });
   return result;
 }
+function checkSumDivide(spreadsheet) {
+  let rows = spreadsheet.split("x");
+  let result = 0;
+  rows.forEach(row => { 
+     let sum;
+	   let nums = row.split(/\s+/);
+     for (let i=0; i < nums.length - 1; i++) {
+       for (let j = i + 1; j < nums.length; j++) {
+         if (nums[i] % nums[j] === 0) {
+           sum = nums[i] / nums[j];
+         } else {
+           if (nums[j] % nums[i] === 0) {
+             sum = nums[j] / nums[i];
+           }
+         }
+       }
+     }
+     // console.log(result);
+	result = result + sum;
+  });
+  return result;
+}
