61834094cf880f1eaaa3e9e953fe04ed54990395
Michael Quint <mjq8@cornell.edu>
2017-12-10 10:34:43 -0800

    streamprocessing

--- adventofcode/day9.js
+++ adventofcode/day9.js
@@ -1,1 +1,7 @@
- 
+function streamProcessing(file) {
+  let fs = require("fs");
+  const text = fs.readFileSync(file, "utf-8");
+  let splitText = text.split("!"); // but wouldnt work for double !!
+  console.log(splitText);
+}
+streamProcessing('./day9.txt');
