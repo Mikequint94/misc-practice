a600545b326f7636de974d19eeec1de7a5edd6d4
Michael Quint <mjq8@cornell.edu>
2017-12-11 22:35:21 -0800

    test text for character remova; l

--- adventofcode/day9.js
+++ adventofcode/day9.js
@@ -1,7 +1,7 @@
 function streamProcessing(file) {
   let fs = require("fs");
   const text = fs.readFileSync(file, "utf-8");
-  var regex = new RegExp('!+');
+  var regex = new RegExp('!\.');
   let splitText = text.split(regex); // but wouldnt work for double !!
   console.log(splitText);
 }
