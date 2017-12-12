728d92c7d667e419fd7f89932b1e03c9c10941b7
Michael Quint <mjq8@cornell.edu>
2017-12-11 22:45:28 -0800

    removing bracket garbage

--- adventofcode/day9.js
+++ adventofcode/day9.js
@@ -2,9 +2,14 @@ function streamProcessing(file) {
   let fs = require("fs");
   const text = fs.readFileSync(file, "utf-8");
   var regex = new RegExp('!\.');
-  let splitText = text.split(regex);
+  let splitText = text.split(regex); //remove canceled characters
 
-  splitText = splitText.filter(n => n); // but wouldnt work for double !!
+  splitText = splitText.filter(n => n);
+  splitText = splitText.join("");
+
+  var brackets = new RegExp('<\.+>');
+  splitText = splitText.split(brackets); //remove garbafe
+  splitText = splitText.join(""); //remove garbafe
   console.log(splitText);
 }
 
