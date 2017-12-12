f2832c5eb44e27184e1bcb5f66ac904825b3a473
Michael Quint <mjq8@cornell.edu>
2017-12-11 22:27:31 -0800

    adding rexexp

--- adventofcode/day9.js
+++ adventofcode/day9.js
@@ -1,8 +1,10 @@
 function streamProcessing(file) {
   let fs = require("fs");
   const text = fs.readFileSync(file, "utf-8");
-  let splitText = text.split("!"); // but wouldnt work for double !!
+  var regex = new RegExp('!+');
+  let splitText = text.split(regex); // but wouldnt work for double !!
   console.log(splitText);
 }
 
-streamProcessing('./day9.txt');
+// streamProcessing('./day9.txt');
+streamProcessing('./day9-test.txt');
