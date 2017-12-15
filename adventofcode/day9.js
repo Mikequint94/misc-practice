7545c108738efeb0996def0d7289bb462cadb6e5
Michael Quint <mjq8@cornell.edu>
2017-12-14 23:20:26 -0800

    got garbage removal and point addition d9p1

--- adventofcode/day9.js
+++ adventofcode/day9.js
@@ -1,17 +1,35 @@
 function streamProcessing(file) {
   let fs = require("fs");
   const text = fs.readFileSync(file, "utf-8");
-  var regex = new RegExp('!\.');
+  let regex = new RegExp('!\.');
   let splitText = text.split(regex); //remove canceled characters
 
   splitText = splitText.filter(n => n);
   splitText = splitText.join("");
 
-  var brackets = new RegExp('<\.+>');
-  splitText = splitText.split(brackets); //remove garbage
-  splitText = splitText.join(""); //remove garbage
-  console.log(splitText);
+  let brackets = new RegExp('<(.*?)>');
+  splitText = splitText.split(brackets); //filter out garbage
+  let noGarbage = ""; // join only non-garbage back in
+  for (let i = 1; i <= splitText.length; i++) {
+    if (i%2 === 1) {
+      noGarbage += splitText[i - 1];
+    }
+  }
+  console.log(noGarbage);
+  // get points
+  let points = 0;
+  let nestLevel = 0;
+  for (let i = 0; i < noGarbage.length; i++) {
+    if (noGarbage[i] === "{") {
+      nestLevel +=1;
+      points += nestLevel;
+    }
+    if (noGarbage[i] === "}") {
+      nestLevel -=1;
+    }
+  }
+  console.log(points, nestLevel);
 }
 
 // streamProcessing('./day9.txt');
-streamProcessing('./day9-test.txt');
+streamProcessing('./day9.txt');
