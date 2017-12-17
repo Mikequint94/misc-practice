b9da73a4597a78dae8fca29745d5aa4f8b97324b
Michael Quint <mjq8@cornell.edu>
2017-12-17 17:24:31 -0500

    d11p1 first try :)

---
+++ adventofcode/day11.js
@@ -0,0 +1,25 @@
+function hexEd(file) {
+  let fs = require("fs");
+  const text = fs.readFileSync(file, "utf-8");
+  let directions = text.split(",");
+  console.log(directions);
+  let up = 0;
+  let right = 0;
+  directions.forEach(direction => {
+    if (direction.includes('n')) {
+      up +=1;
+    }
+    if (direction.includes('e')) {
+      right +=1;
+    }
+    if (direction.includes('s')) {
+      up -=1;
+    }
+    if (direction.includes('w')) {
+      right -=1;
+    }
+  });
+  console.log(up, right);
+}
+
+hexEd('./day11.txt');
