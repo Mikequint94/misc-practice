cce2c16b00b51c29ad1ef0145da3599d9371fc7a
Michael Quint <mjq8@cornell.edu>
2017-12-18 02:11:52 -0500

    d12p1 complete.  recursively added children to all groups

--- adventofcode/day12.js
+++ adventofcode/day12.js
@@ -2,8 +2,40 @@ function digPlumb(file) {
   let fs = require("fs");
   const text = fs.readFileSync(file, "utf-8");
   let lines = text.split("\n");
-  
-  console.log(lines);
+  lines.splice(lines.length-1, 1);
+  allGroups = new Set('0');
+  dictionary = new Map;
+  lines.forEach(line => {
+    let splitLine = line.split(" <-> ");
+    let program = splitLine[0];
+    let groups = splitLine[1];
+    // console.log(program, groups);
+    // groups.split(", ").forEach(group => {
+      dictionary.set(program, groups.split(", "));
+    // });
+  });
+  // let i = 0;
+  // while (i < 5) {
+    // let lastEl = allGroups[allGroups.length - 1];
+    let newAdds = dictionary.get('0');
+    newAdds.forEach(child => {
+      addChildren(child);
+    });
+    console.log(allGroups.size);
+  //   i++;
+  // }
 }
-
+function addChildren(child) {
+    allGroups.add(child);
+    // console.log(allGroups);
+    let newAdds = dictionary.get(child);
+    // console.log(newAdds);
+    if (newAdds) {
+    newAdds.forEach(group => {
+      if (!allGroups.has(group)) {
+        addChildren(group);
+      }
+    });
+  }
+}
 digPlumb('./day12.txt');
