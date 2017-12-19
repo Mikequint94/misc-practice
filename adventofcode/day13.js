3acd563f1cfde6fdd588b79cf9278ce2c4121a85
Michael Quint <mjq8@cornell.edu>
2017-12-19 16:39:22 -0500

    make map from input

--- adventofcode/day13.js
+++ adventofcode/day13.js
@@ -2,6 +2,17 @@ function fireWall(file) {
   let fs = require("fs");
   const text = fs.readFileSync(file, "utf-8");
   let lines = text.split("\n");
+  lines.splice(-1);
+  let wall = new Map();
+  lines.forEach(line => {
+    line = line.split(": ");
+    wall.set(line[0],line[1]);
+  });
+
+Array.from(wall.keys()).forEach(key => {
+  console.log(key);
+});
+
 }
 
 fireWall('./day13-test.txt');
