b9f58322211baf9452c0ab69f1c6f1eb61978d64
Michael Quint <mjq8@cornell.edu>
2017-12-24 02:38:13 -0500

    d15p2

--- adventofcode/day15.js
+++ adventofcode/day15.js
@@ -1,6 +1,4 @@
 function generatorMatch(genAstart, genBstart) {
-  let sequencesA=[];
-  let sequencesB=[];
   let lastA = genAstart;
   let lastB = genBstart;
   let newA;
@@ -23,5 +21,37 @@ function generatorMatch(genAstart, genBstart) {
   console.log(count);
 
 }
+function generatorMatch2(genAstart, genBstart) {
+  let sequencesA=[];
+  let sequencesB=[];
+  let lastA = genAstart;
+  let lastB = genBstart;
+  let newA;
+  let newB;
+  let count = 0;
 
-generatorMatch(512, 191);
+  while (sequencesA.length < 5000000 || sequencesB.length < 5000000) {
+    newA = (lastA * 16807) % 2147483647;
+    newB = (lastB * 48271) % 2147483647;
+    lastA = newA;
+    lastB = newB;
+    if (newA % 4 === 0) {
+      newA = newA.toString(2);
+      newA = newA.split("").splice(-16).join("");
+      sequencesA.push(newA);
+    }
+    if (newB % 8 === 0) {
+      newB = newB.toString(2);
+      newB = newB.split("").splice(-16).join("");
+      sequencesB.push(newB);
+    }
+  }
+  for (let i = 0; i < 5000000; i++) {
+    if (sequencesA[i] === sequencesB[i]) {count++;}
+  }
+  console.log(count);
+
+}
+
+generatorMatch2(65, 8921);
+generatorMatch2(512, 191);
