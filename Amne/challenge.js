01bb59c70acda2fced0eb86568eecdbd0cf46bac
Michael Quint <mjq8@cornell.edu>
2018-01-02 23:56:30 -0500

    works for iall 2 day windows

--- Amne/challenge.js
+++ Amne/challenge.js
@@ -2,8 +2,24 @@ function windowPatterns(file) {
   let fs = require("fs");
   const text = fs.readFileSync(file, "utf-8");
   let lines = text.split("\n");
-  // lines.splice(-1);
-  console.log(lines);
+  let numDays = parseInt(lines[0].split(" ")[0]);
+  let windowSize = parseInt(lines[0].split(" ")[1]);
+  let prices = lines[1].split(" ").map(Number);
+  console.log(prices, windowSize, numDays);
+  
+  let output = [];
+  for (let i = 0; i < numDays - windowSize + 1; i++) {
+    let pattern = 0;
+    for (let j = 0; j < windowSize - 1; j++) {
+      if (prices[i + j] < prices[i + j + 1]) {
+        pattern += 1;
+      } else if (prices[i + j] > prices[i + j + 1]) {
+        pattern -= 1;
+      }
+    }
+    output.push(pattern);
+  }
+  console.log(output);
 }
 
 windowPatterns("./test.txt")
