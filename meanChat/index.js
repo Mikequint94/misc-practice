ef6fb6db903c939698526c8e80d43e82827258b1
Michael Quint <mjq8@cornell.edu>
2018-01-05 00:33:10 -0500

    adding username

--- meanChat/index.js
+++ meanChat/index.js
@@ -1,9 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
