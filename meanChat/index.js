41ed52b95eba740d4910d8ae5813dd24dfd75f68
Michael Quint <mjq8@cornell.edu>
2018-01-10 23:39:48 -0800

    adding online user fn

--- meanChat/index.js
+++ meanChat/index.js
@@ -11,24 +11,26 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  console.log(io.engine.clientsCount/2);
   let user = null;
   socket.on('chat message', function(msg){
     io.emit('chat message', user + " : " + msg);
   });
-  
+
   socket.on('set user', function(msg){
     user = msg;
     io.emit('chat message', user + " has joined the chat!");
+    io.emit('new user', user);
   });
-  
+
   socket.on('typing', function(typer){
     io.emit('typing', typer + " is typing");
   });
-  
+
   socket.on('stoptyping', function(typer){
     io.emit('stoptyping', typer);
   });
-  
+
   socket.on('disconnect', function(){
     if (user) {
       io.emit('chat message', user + " has left the chat!");
