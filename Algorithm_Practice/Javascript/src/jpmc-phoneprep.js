8371eee0a02e63c544dd7598f884e77e9727ac38
Michael Quint <mjq8@cornell.edu>
2018-01-12 13:44:15 -0800

    prep for jpmc phone screen

--- Algorithm_Practice/Javascript/src/jpmc-phoneprep.js
+++ Algorithm_Practice/Javascript/src/jpmc-phoneprep.js
@@ -12,12 +12,26 @@ function filterCustomers() {
   let filtered = customers.filter((customer) => (
     customer.month === "september"// && customer.last_purchase > new Date(2017,3,3)
   ));
-
   console.log(filtered.map((el) => el.id));
 }
-
 filterCustomers();
+
 // Given the HTML and CSS add an event to the div using JS – a quick event
+
+function modifyText() {
+  let t2 = document.getElementById("t2");
+    t2.firstChild.nodeValue = "three";
+}
+
+let el = document.getElementById("outside");
+el.addEventListener("click", modifyText);
+//also keydown, mouseover, etc.
+
 // If provided a set of functions why would this code break or why it didn’t work?
 // It was centered around asynchronous code and he would call a certain variable
 // If this was returned in a promise, how would you do it?
+
+fetch("/messages")
+    .then(function(info) {
+      info.json().then(data => console.log(data));
+    });
