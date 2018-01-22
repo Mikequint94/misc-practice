b7ccd0e0f5ef217698caaefb676fd95a66984df6
Michael Quint <mjq8@cornell.edu>
2018-01-21 23:20:15 -0800

    test making node module

---
+++ testModule/index.js
@@ -0,0 +1,6 @@
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
+exports.heartBreak = () => {
+  console.log("THIS IS HOW TO BE A HEART BREAKKERRR");
+}
