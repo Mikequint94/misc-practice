9ac4209b002c4b02554b2718bcde0ea9b687f343
Michael Quint <mjq8@cornell.edu>
2018-01-24 09:48:45 -0800

    pre es6 solution to closure question

--- Algorithm_Practice/Javascript/src/enteloq.js
+++ Algorithm_Practice/Javascript/src/enteloq.js
@@ -1,6 +1,37 @@
+//actually all learned at JPMC interview
+
 var theFinalCountDown = [1, 2, 3, 4, 5];
 
+//original. Prints undefined 5 times.
+// for (var i = 0; i < theFinalCountDown.length; i++) {
+//    setTimeout(function() {
+//      console.log(theFinalCountDown[i]);
+//    }, 1000);
+// }
+
+
+//easy ES6 fix.  Change var to let;
+// for (let i = 0; i < theFinalCountDown.length; i++) {
+//    setTimeout(function() {
+//      console.log(theFinalCountDown[i]);
+//    }, 1000);
+// }
+
+
+//OG fix using closure.  Need parentheses.  and to call it with i.
 for (var i = 0; i < theFinalCountDown.length; i++) {
-   let count = theFinalCountDown[i];
-   setTimeout(() => {console.log(count);},  count*1000);
+  (function(j) {
+    setTimeout(function() {
+        console.log(theFinalCountDown[j]);
+      }, 1000);
+    })(i);
 }
+
+//also learned if you do
+function hello () {
+  var a = b = 4
+}
+hello();
+
+// console.log(a);  a will be undefined.
+// console.log(b);  b will be for because it is set on global scope.
