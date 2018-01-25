ea66df082c8a45c845bb595ce2577f6e7ca5545c
Michael Quint <mjq8@cornell.edu>
2018-01-24 16:14:37 -0800

    ruby reminders

---
+++ Algorithm_Practice/Ruby/lib/refex-prep.rb
@@ -0,0 +1,19 @@
+# Review Rails!
+
+# Ruby Blocks
+
+# Procs and Lambdas
+
+# Closures
+
+# Ways to call a method
+
+# N+1 Queries and how to optimize
+
+# Public / Private / Protected Methods
+
+# Object Oriented Programming
+
+# What is a load balancer?
+
+# How do shards work?
