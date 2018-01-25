623662debce1c8b1904289093a8829dc18ee1c1e
Michael Quint <mjq8@cornell.edu>
2018-01-24 17:58:50 -0800

    ruby methods, oop

--- Algorithm_Practice/Ruby/lib/refex-prep.rb
+++ Algorithm_Practice/Ruby/lib/refex-prep.rb
@@ -12,8 +12,27 @@
 
 # Public / Private / Protected Methods
 
+## Public:  can call whenever. Defaults
+
+## Private:
+## method can only be called within a class where it was defined.
+## Like from another method within the class.
+
+## Protected:
+## same.  Only used internally within object.
+## can do self.method_name and it will work.  or call that method on other instances
+## of the same class.
+
 # Object Oriented Programming
+## Programming Paradigm based on the concept of objects.
+## class based, objects are instances of classes.  Java, Python, Ruby, etc.
 
 # What is a load balancer?
+## Distributing incoming network traffic across multiple backend servers.
+## Efficiently reroutes traffic only to servers that are online and aren't overworked.
+## Could distribute round robin, or by least connections, or by hashing IP address.
 
 # How do shards work?
+## Sharding is database partitioning to separate large databases into smaller faster shards.
+## Data is not repeated across shards and spread across different servers.
+## Split maybe based on geographic data or something concrete is easier.
